5b2c987728de6b21101209f98837f00ac1708c59
shenchaommm <260910853@qq.com>
2016-07-29 21:59:28 +0800

    bbb

--- php/email.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$q=$_REQUEST["value"];
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-$headers .= "From:shenchaommm@163.com" . "\r\n";
-mail("260910853@qq.com", "WeatherWin8", $q, $headers);
-echo $q;
-?>
