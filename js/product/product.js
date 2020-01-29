77acd7ccc4cf6c0490aa1f2d8a8e4f531dce7829
shenchaommm <260910853@qq.com>
2020-01-29 16:54:18 +0800

    4

--- js/product/product.js
+++ js/product/product.js
@@ -7,15 +7,25 @@ function initProduct(){
     if(!productdata) return;
     //if(!Array.isArray(productdata)) return;
     //if(productdata.length<3) return;
-
+    
     document.getElementsByTagName("title")[0].innerText=productdata.title;
     var productimagebanner=document.getElementById("productimagebanner");
     var productimagespan=document.getElementById("productimagespan");
     var productlink=document.getElementById("productlink");
     var productatt=document.getElementById("productatt");
     var tblink="https://item.taobao.com/item.htm?id="+productdata.id;
-    document.getElementById("productimageleft").addEventListener("click",onimagebannershow,false);
-    document.getElementById("productimageright").addEventListener("click",onimagebannershow,false);
+    var support=1;
+    if(!CSS.supports("(scroll-snap-type: x mandatory)")){
+        support=0;
+        productimagebanner.style.width=(productdata.img.length)*100+"vw";
+        document.getElementById("productimageleft").addEventListener("click",onimagebannershow1,false);
+        document.getElementById("productimageright").addEventListener("click",onimagebannershow1,false);
+    }
+    else{
+        support=1;
+        document.getElementById("productimageleft").addEventListener("click",onimagebannershow,false);
+        document.getElementById("productimageright").addEventListener("click",onimagebannershow,false);
+    }
     //productimagebanner.style.width=(productdata.img.length)*100+"vw";
     for(i=0;i<productdata.img.length;i++){
         var tblinkimg="https://img.alicdn.com/imgextra/i1/2200740969161/"+productdata["img"][i]+"_!!2200740969161.jpg";
@@ -24,14 +34,16 @@ function initProduct(){
         var imgdiv=document.createElement("li");
         imgdiv.appendChild(img);
         productimagebanner.appendChild(imgdiv);
-        
-        var span=document.createElement("span");
-        if(i==0){
-            span.className="imagebannerdotshow";   
+        //if()
+        if(support==0){
+            var span=document.createElement("span");
+            if(i==0){
+                span.className="imagebannerdotshow";   
+            }
+            span.setAttribute("data-num",i);
+            span.addEventListener("click",onimagebannershow1,false);
+            productimagespan.appendChild(span);
         }
-        span.setAttribute("data-num",i);
-        span.addEventListener("click",onimagebannershow,false);
-        //productimagespan.appendChild(span);
     }
     var p=document.getElementById("producttitle");
     p.innerText=productdata.title;
@@ -68,7 +80,7 @@ function onimagebannershow(){
     else if(num=="f"){
         //direction="f"
         nownum=ul.scrollLeft+ul.clientWidth;
-        if(nownum>(li.length-1)*ul.clientWidth)nownum=0;
+        if(nownum>(li.length-0.5)*ul.clientWidth)nownum=0;
     }else{
         //if(nownum<num) direction="f";
         //nownum=num;
@@ -76,4 +88,29 @@ function onimagebannershow(){
     //alert(nownum);
     ul.scrollLeft=nownum;
     //span[nownum].className="imagebannerdotshow";
+}
+function onimagebannershow1(){
+    var num=this.getAttribute("data-num");
+    var nowshow=document.getElementsByClassName("imagebannerdotshow")[0];
+    var nownum=nowshow.getAttribute("data-num");
+    if(num==nownum) return;
+    nowshow.className="";
+    var span=document.getElementById("productimagespan").getElementsByTagName("span");
+    var ul=document.getElementById("productimagebanner");
+
+    var direction="b";
+    if(num=="b"){
+        nownum--;
+        if(nownum<0) nownum=span.length-1;
+    }
+    else if(num=="f"){
+        direction="f"
+        nownum++;
+        if(nownum>span.length-1)nownum=0;
+    }else{
+        if(nownum<num) direction="f";
+        nownum=num;
+    }
+    ul.style.left="-"+nownum*100+"vw";
+    span[nownum].className="imagebannerdotshow";
 }
