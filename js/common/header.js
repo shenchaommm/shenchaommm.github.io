8febed4ec0a6e4dfed8f6064f0e886a56c57994d
shenchaommm <260910853@qq.com>
2020-02-02 19:26:41 +0800

    6
    version6

--- js/common/header.js
+++ js/common/header.js
@@ -1,8 +1,8 @@
 function headerContent(){
-    var header='<div class="header">'+
+    var header='<div class="header maxwidth">'+
     '<div id="manu" class="manu_left">'+
         '<button class="manu_button nav_button" id="manubutton"><span class="iconfont icon-menu" id="menuiconfont" data-menutype="menu"></span></button>'+
-        '<ul id="menulist" class="menulist"></ul>'+
+        '<ul id="menulist" class="menulist maxwidth"></ul>'+
     '</div>'+    
     '<div class="logo logo_middle">'+
         '<a href="index.html"><img src="image/logo/logo.png"/></a>'+
