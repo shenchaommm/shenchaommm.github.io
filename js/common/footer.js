4a16032511e48182c2f4367d6eb28c7278be6d40
shenchaommm <260910853@qq.com>
2020-02-07 15:29:20 +0800

    event

--- js/common/footer.js
+++ js/common/footer.js
@@ -1,15 +1,15 @@
 function footerContent(){
     var footer='<div class="footer">'+   
     '<div class="footermenu">'+
-        '<a href="index.html"><img src="image/logo/logo.png"/></a>'+
-        '<a href="event.html?id=4">使用条款</a>'+
+        '<div><a href="index.html"><img src="image/logo/logo.png"/></a></div>'+
+        '<div><a href="event.html?id=4">使用条款</a>'+
         '<a href="event.html?id=5">隐私与Cookie</a>'+
         '<a href="aboutcontact.html">联系我们</a>'+
         '<a href="productslist.html">我们的产品</a>'+
-        '<a href="https://shop174208588.taobao.com/">我们的网店</a>'+
+        '<a href="about.html">关于我们</a></div>'+
     '</div>'+
     '<div class="footerfoot">'+
-        '<a>2020创思照明</a>'+
+        '<a>©2020创思照明</a>'+
     '</div>'+
 '</div>';
     return footer;
